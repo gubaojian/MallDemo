27977f02a6b4347504eea5c6ebefd1f1db1ca0dc
baojian.gu <gubaojian@163.com>
2025-04-17 20:10:18 +0800

    拉伸策略修改

--- mall/app/src/main/java/com/zwb/mvvm_mall/ui/home/GlideBannerImageAdapter.kt
+++ mall/app/src/main/java/com/zwb/mvvm_mall/ui/home/GlideBannerImageAdapter.kt
@@ -18,7 +18,7 @@ class GlideBannerImageAdapter (imageUrls: List<BannerResponse>) : BannerAdapter<
         val imageView = ImageView(parent!!.context)
         val params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
         imageView.layoutParams = params
-        imageView.scaleType = ImageView.ScaleType.CENTER
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
         //通过裁剪实现圆角
         BannerUtils.setBannerRound(imageView, 20f)
         return ImageHolder(imageView)
